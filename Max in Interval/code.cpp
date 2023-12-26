280ba704dc3eda7e896a03ad4973613859ca78a5
Udrasht_pal <udrasht.pal@students.iiit.ac.in>
2023-12-26 23:50:52 +0530

    find maximum in given range

---
+++ Max in Interval/code.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+class SegmentTree
+{
+
+private:
+    // create the segment tree
+    void build(int node, int start, int end, vector<int> &arr)
+    {
+        if (start == end)
+        {
+            tree[node] = arr[start];
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int left = node * 2;
+            int right = node * 2 + 1;
+            build(left, start, mid, arr);
+            build(right, mid + 1, end, arr);
+            tree[node] = max(tree[left], tree[right]);
+        }
+    }
+
+
+    // fild the maximum in the given range [left, right]
+    // time complaxity O(logn)
+    int queryprocess(int node, int start, int end, int left, int right)
+    {
+        if (end < left || right < start)
+        {
+            return INT_MIN;
+        }
+
+        else if (start == end)
+        {
+            return tree[node];
+        }
+        else if (left <= start && end <= right)
+        {
+            return tree[node];
+        }
+        else
+        {
+            int mid = (start + end) / 2;
+            int leftValue = queryprocess(2 * node, start, mid, left, right);
+            int rightValue = queryprocess(2 * node + 1, mid + 1, end, left, right);
+            return max(leftValue, rightValue);
+        }
+    }
+
+public:
+    vector<int> tree;
+    int n;
+
+    // constructoe
+    SegmentTree(vector<int> &arr)
+    {
+        n = arr.size();
+        tree.resize(4 * n, INT_MIN);
+        build(1, 0, n - 1, arr);
+       
+    }
+
+
+
+    int query(int left, int right)
+    {
+        return queryprocess(1, 0, n - 1, left, right);
+    }
+};
+int main()
+{
+    vector<int> arr;
+    arr = {8, 7, 4, 2, 5, 3, 1, 10};
+    // arr={8,7,4,2,5};
+    SegmentTree st(arr);
+    vector<vector<int>> querys;
+    querys = {{0, 7}, {0, 3}, {2, 7}, {1, 6}, {2, 6}};
+    // querys={{0,4},{0,3},{2,4},{3,4},{2,3}};
+    for (int i = 0; i < querys.size(); i++)
+    {
+        cout << "(" << querys[i][0] << ", " << querys[i][1] << ") -> " << st.query(querys[i][0], querys[i][1]) << endl;
+    }
+    return 0;
+}
